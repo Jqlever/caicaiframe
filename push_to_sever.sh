5750a0b2d16df14ac2707607a82e1fbaaea8c7b0
Jqlever <31581888+Jqlever@users.noreply.github.com>
2020-05-20 18:00:35 +0800

    Update push_to_sever.sh

--- push_to_sever.sh
+++ push_to_sever.sh
@@ -1,3 +1,5 @@
 scp /opt/middleware/jenkins/workspace/caicaiframe/target/spring-mongodb-sample-0.0.1-SNAPSHOT.jar root@172.31.161.83:/opt/jar/test
+ssh root@172.31.161.83
+cd /opt/jar/test
 ##
 ./restart.sh
