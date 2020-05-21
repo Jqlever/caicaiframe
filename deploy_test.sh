71681de0cf65d3dcd8d49493f007587b90ab2af4
Jqlever <31581888+Jqlever@users.noreply.github.com>
2020-05-21 13:11:14 +0800

    Update deploy_test.sh

--- deploy_test.sh
+++ deploy_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #/usr/bin/docker build -t spring-mongodb-sample:0.0.1-SNAPSHOT .
 #docker run -d --name test spring-mongodb-sample:0.0.1-SNAPSHOT
-java -jar target/spring-mongodb-sample:0.0.1-SNAPSHOT.jar
+java -jar t/var/jenkins_home/workspace/caicaiframe/target/spring-mongodb-sample-0.0.1-SNAPSHOT.jar
