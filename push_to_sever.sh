56961d8d06224c785ee7917cdd88e4aba86a04fc
Jqlever <31581888+Jqlever@users.noreply.github.com>
2020-05-21 10:03:22 +0800

    Update push_to_sever.sh

--- push_to_sever.sh
+++ push_to_sever.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ssh root@172.31.161.83 "scp root@172.31.161.96:/opt/middleware/jenkins/workspace/caicaiframe/target/spring-mongodb-sample-0.0.1-SNAPSHOT.jar /opt/jar/test"
-ssh root@172.31.161.83 "nohup /home/java/jdk1.8.0_171/bin/java -jar /opt/jar/test/spring-mongodb-sample-0.0.1-SNAPSHOT.jar &"
+ssh root@172.31.161.83
+scp root@172.31.161.96:/opt/middleware/jenkins/workspace/caicaiframe/target/spring-mongodb-sample-0.0.1-SNAPSHOT.jar /opt/jar/test
+nohup /home/java/jdk1.8.0_171/bin/java -jar /opt/jar/test/spring-mongodb-sample-0.0.1-SNAPSHOT.jar &
